81fe813db6e1858e7799c07ea6d2f74b061201b3
Katyusha <65454734+katyusha-pixis@users.noreply.github.com>
2021-07-15 09:50:16 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+const Discord  = require('discord.js');
+const client   = new Discord.Client();
+const Canvas = require('canvas');
+const fetch = require('node-fetch')
+
+Canvas.registerFont('ConcertOne-Regular.ttf', { family: 'Concert One' })
+
+client.on('ready' , async () => {
+
+    console.log('bot ready');
+
+    setInterval( async () => {
+        let guild = client.guilds.cache.get("847117485975011348")
+        const canvas = Canvas.createCanvas(2920,1642);
+        const context = canvas.getContext('2d');
+        const background = await Canvas.loadImage('LINK_IMAGE');
+        
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+        
+        context.strokeRect(0, 0, canvas.width, canvas.height);
+        
+
+        context.font = '130px "Concert One"';
+
+
+        context.fillStyle = '#DDFF00';
+
+        context.fillText(`${ guild.memberCount }`, 689 ,1029 );
+        context.fillText(`HIGHLIGHT STAFF`, 1041 ,1433 );
+
+        let i;
+        let channels = guild.channels.cache.filter(channel => channel.type === "voice")
+        channels.map(channel => {
+            i =+ channel.members.size
+        });
+
+        context.fillText(i, 517 ,1161 );
+
+       let res = await fetch('https://api.codebazan.ir/time-date/?td=dateen')
+       let data = await res.text()
+
+        context.fillText(data, 569 , 1293 )
+
+    
+        await guild.setBanner(canvas.toBuffer())
+    
+}, 20000);
+
+});
+
+client.on("ready", () => {
+    client.user.setActivity('Developed By Katusha')
+      const channel = client.channels.cache.get("853185432531894292");
+      if (!channel) return console.error("The channel does not exist!");
+      channel.join().then(connection => {
+          console.log("Successfully connected.");
+      }).catch(e => {
+          console.error(e);
+      });
+  });
+
+
+client.login("YOUR_BOT_TOKEN");
